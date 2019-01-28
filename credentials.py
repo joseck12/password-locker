c19025ecb93a06af985144304ca836ea472abf76
joseck ogachi <jogachi4@gmail.com>
2019-01-28 15:40:54 +0300

    add spacing and indentation to .py files

--- credentials.py
+++ credentials.py
@@ -1,3 +1,5 @@
+import unittest
+from user import User
 class Credentials:
     """
     class to create account credentials,generate and save password information
--- test_user.py
+++ test_user.py
@@ -91,5 +91,5 @@ class TestUser(unittest.TestCase):
 
        self.assertEqual(self.new_user.username,pyperclip.paste())
 
-    if __name__ == '__main__':
-        unittest.main()
+if __name__ == '__main__':
+   unittest.main()
