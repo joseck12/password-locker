c2a46ffe5dfda9de0c821d71450eaf09d181a4e5
joseck ogachi <jogachi4@gmail.com>
2019-01-28 09:58:07 +0300

    add changes to user_test.py file

--- user_test.py
+++ user_test.py
@@ -25,3 +25,22 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.account,"Tweeter")
         self.assertEqual(self.new_user.username,"Mercy")
         self.assertEqual(self.new_user.password,"0000")
+
+        def test_save_multiple_user(self):
+            '''
+            test_save_multiple_contact to check if we can save multiple user
+            objects to our contact_list
+            '''
+            self.new_user.save_user()
+            test_user = UserUser("Tweeter", "Mercy", "0000") # new user
+            test_user.save_user()
+            self.assertEqual(len(User.user_list),2)
+    # setup and class creation up here
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+    if __name__ == '__main__':
+    unittest.main()
