de58dfdf2bf88ea12798dd0ccfa689817af470c4
joseck ogachi <jogachi4@gmail.com>
2019-01-28 12:13:04 +0300

    add changes to .py files

--- user.py
+++ user.py
@@ -1,10 +1,17 @@
 class User:
 
     user_list = []
-    def __init__(self,account,user_name,password):
+
+    def __init__(self,account,username,password):
+
+     def save_user(self):
+
+            '''
+            save_user method saves user objects into user_list
+            '''
+
+            User.user_list.append(self)
 
             self.account = account
             self.username = username
             self.password = password
-
-            contact_list = [] # Empty contact list
--- user_test.py
+++ user_test.py
@@ -1,5 +1,5 @@
 import unittest # Importing the unittest module
-import random
+# import pyperclip
 from user import User # Importing the user class
 
 class TestUser(unittest.TestCase):
@@ -17,15 +17,16 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("Tweeter", "Mercy", "0000") # create user object
 
+
     def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
 
-        self.assertEqual(self.new_user.account,"Tweeter")
+        self.assertEqual(self.new_User.account,"Tweeter")
         self.assertEqual(self.new_user.username,"Mercy")
         self.assertEqual(self.new_user.password,"0000")
-
+#
     def test_save_multiple_user(self):
             '''
             test_save_multiple_contact to check if we can save multiple user
@@ -37,11 +38,7 @@ class TestUser(unittest.TestCase):
             self.assertEqual(len(User.user_list),2)
     # setup and class creation up here
 
-    def tearDown(self):
-            '''
-            tearDown method that does clean up after each test case has run.
-            '''
-            User.user_list = []
+
     def test_save_user(self):
         '''
         test_save_user test case to test if the user object is saved into
@@ -49,15 +46,6 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user.save_user() # saving the new user
         self.assertEqual(len(User.user_list),1)
-
-    def test_user_exists(self):
-        self.new_user.save_user()
-        test_user = User("Tweeter", "Mercy", "0000")
-        test_user.save_user()
-
-        user_exists = User.user_exist("Tweeter")
-
-        self.assertTrue(user_exists)
 
     def test_delete_user(self):
             self.new_user.save_user()
@@ -80,5 +68,33 @@ class TestUser(unittest.TestCase):
 
         self.assertEqual(found_user.username,test_user.username)
 
-    if __name__ == '__main__':
+
+    def test_user_exists(self):
+        self.new_user.save_user()
+        test_user = User("Tweeter", "Mercy", "0000")
+        test_user.save_user()
+
+        user_exists = User.user_exist("Tweeter")
+
+        self.assertTrue(user_exists)
+
+    def test_display_all_users(self):
+        '''
+        method that returns a list of all users saved
+        '''
+
+        self.assertEqual(User.display_users(),User.user_list)
+
+      def test_copy_username(self):
+        '''
+        Test to confirm that we are copying the username from a found user
+        '''
+
+        self.new_user.save_user()
+        User.copy_username("Mercy")
+
+        self.assertEqual(self.new_user.username,pyperclip.paste())
+
+
+if __name__ == '__main__':
     unittest.main()
