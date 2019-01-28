177f5322e95ac3a540a1003d529438fcae5b5f21
joseck ogachi <jogachi4@gmail.com>
2019-01-28 09:37:56 +0300

    additional content to user_test.py file

--- user_test.py
+++ user_test.py
@@ -16,3 +16,12 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("Tweeter", "Mercy", "0000") # create user object
+
+        def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_user.account,"Tweeter")
+        self.assertEqual(self.new_user.username,"Mercy")
+        self.assertEqual(self.new_user.password,"0000")
