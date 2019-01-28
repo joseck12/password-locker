e63603aecdf2ee05212c807599711da11c184034
joseck ogachi <jogachi4@gmail.com>
2019-01-28 17:44:59 +0300

    add details to README.md

--- README.md
+++ README.md
@@ -1,22 +1,46 @@
+## Password-locker
+A python application that allows generating and storing of passwords for various accounts.
 
-MIT License
+## Author Name
 
-Copyright (c) 2019 joseck12
+Joseck Ogachi
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+# Project Description
+This project will enable users to generate passwords for various accounts and the details stored. Users can generate one or multiple accounts.
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+## Technology
+ This project runs on python3.6
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE
+### Prerequisites
+
+To install this project you need the following:
+
+```* A Github account```
+
+```* A Terminal```
+
+```*Pyhon3.6```
+
+## Setup and Installation
+
+You must have a Github account,then follow the link below
+
+```https://github.com/joseck12/password-locker```
+
+ On your terminal type 'git clone' followed by the link below then run the command so as to get the project on your local computer.
+
+```https://github.com/joseck12/password-locker.git```
+
+Open the project on your terminal and start the python shell
+
+### License
+
+MIT licence
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Copyright (c) 2018
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3.6
 from user import User
-
- def create_account(account,username,password):
+def create_account(account,username,password):
     '''
     Function to create a new account
     '''
@@ -40,8 +39,62 @@ def display_users():
 
 def main():
     print("Hello Welcome to Password Locker. What is your name?")
-            user_name = input()
-             print(f"Hello {user_name}. what would you like to do?")
-            print('\n')
-            while True:
+    user_name = input()
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+    while True:
                     print("Use these short codes : cc - create a new account, dc - display users, fc -find a user, ex -exit the Password Locker")
+                    short_code = input().lower()
+
+                    if short_code == 'cc':
+                            print("Account name")
+                            print("-"*10)
+
+                            print ("User name ....")
+                            f_name = input()
+
+                            print("Password ...")
+                            l_name = input()
+                            save_users(create_account(Instagram, username, password)) # create and save new contact.
+                            print ('\n')
+                            print(f"New User {username} created")
+                            print ('\n')
+
+
+                    elif short_code == 'dc':
+
+                            if display_users():
+                                    print("Here is a list of all your users")
+                                    print('\n')
+
+                                    for user in display_users():
+                                            print(f"{user.username}")
+
+                                    print('\n')
+                            else:
+                                    print('\n')
+                                    print("You dont seem to have any users saved yet")
+                                    print('\n')
+
+                    elif short_code == 'fc':
+
+                            print("Enter the username you want to search for")
+
+                            search_username = input()
+                            if check_existing_users(search_username):
+                                    search_user = find_user(search_username)
+                                    print(f"{search_user.username}")
+                                    print('-' * 10)
+
+                                    # print(f"Phon.......{search_contact.phone_number}")
+                                    # print(f"Email address.......{search_contact.email}")
+                            else:
+                                    print("That user does not exist")
+
+                    elif short_code == "ex":
+                            print("Bye .......")
+                            break
+                    else:
+                            print("I really didn't get that. Please use the short codes")
+if __name__ == '__main__':
+    main()
