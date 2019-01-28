5644c330585224b64d3d1d2cfc50dd709f66c525
joseck ogachi <jogachi4@gmail.com>
2019-01-28 10:32:45 +0300

    add new changes to user_test.py file

--- user_test.py
+++ user_test.py
@@ -17,7 +17,7 @@ class TestUser(unittest.TestCase):
         '''
         self.new_user = User("Tweeter", "Mercy", "0000") # create user object
 
-        def test_init(self):
+    def test_init(self):
         '''
         test_init test case to test if the object is initialized properly
         '''
@@ -26,7 +26,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.new_user.username,"Mercy")
         self.assertEqual(self.new_user.password,"0000")
 
-        def test_save_multiple_user(self):
+    def test_save_multiple_user(self):
             '''
             test_save_multiple_contact to check if we can save multiple user
             objects to our contact_list
@@ -36,20 +36,49 @@ class TestUser(unittest.TestCase):
             test_user.save_user()
             self.assertEqual(len(User.user_list),2)
     # setup and class creation up here
+
     def tearDown(self):
             '''
             tearDown method that does clean up after each test case has run.
             '''
             User.user_list = []
+    def test_save_user(self):
+        '''
+        test_save_user test case to test if the user object is saved into
+         the user list
+        '''
+        self.new_user.save_user() # saving the new user
+        self.assertEqual(len(User.user_list),1)
+
+    def test_user_exists(self):
+        self.new_user.save_user()
+        test_user = User("Tweeter", "Mercy", "0000")
+        test_user.save_user()
+
+        user_exists = User.user_exist("Tweeter")
+
+        self.assertTrue(user_exists)
 
     def test_delete_user(self):
+            self.new_user.save_user()
+            test_user = User("Tweeter", "Mercy", "0000") # new user
+            test_user.user()
+
+            self.new_user.delete_user()# Deleting a user object
+            self.assertEqual(len(User.user_list),
+
+    def test_find_user_by_account(self):
+        '''
+        test to check if we can find a user by account and display information
+        '''
 
             self.new_user.save_user()
             test_user = User("Tweeter", "Mercy", "0000") # new user
             test_user.user()
 
-            self.new_contact.delete_contact()# Deleting a contact object
-            self.assertEqual(len(Contact.contact_list),
+        found_user = User.find_by_account("Tweeter")
+
+        self.assertEqual(found_user.username,test_user.username)
 
     if __name__ == '__main__':
     unittest.main()
